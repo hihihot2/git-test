4bbf3f1fbb4355cd8b11faacf445b6a8117371c9
조규환 <17679@hanmail.net>
2019-09-15 16:15:40 +0900

    [func] [기능 추가] (#1)
    * [func003] cors 기능 추가
    
    * [func004] bodyParser 기능 추가

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ console.log('index.js create');
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser());
+
 app.listen(80, () => {
     console.log('server started on port 80');
 });
