7299872a49051ee3ca64ccfc74d11d76de6f9e7f
조규환 <kyuhcho@smilegate.com>
2019-09-15 15:32:26 +0900

    [func] bodyParser 기능 추가

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ console.log('index.js create');
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser());
+
 app.listen(80, () => {
     console.log('server started on port 80');
 });
