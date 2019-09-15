909fb54b9104d44dcfe13568feac0ca40acfc6af
조규환 <kyuhcho@smilegate.com>
2019-09-15 15:29:41 +0900

    [func] cors 기능 추가

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ console.log('index.js create');
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
+app.use(cors());
+
 app.listen(80, () => {
     console.log('server started on port 80');
 });
